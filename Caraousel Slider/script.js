4827dec36cce5b2c02ba8ce2ad64660ab5249e8e
Aniruddha Kadam <aniruddhadroit@gmail.com>
2024-08-15 21:30:48 +0530

    Index - trying swiper js

---
+++ Caraousel Slider/script.js
@@ -0,0 +1,32 @@
+var swiper = new Swiper(".swiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  initialSlide: 2,
+  speed: 600,
+  loop:true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 80,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  on: {
+    click(event) {
+      swiper.slideTo(this.clickedIndex);
+    },
+  },
+  pagination: {
+    el: ".swiper-pagination"
+  },
+  autoplay:{
+    delay:1000,
+    disableOnInteraction:false,
+    pauseOnMouseEnter: true,
+    // autoplayPause(swiper){
+
+    // }
+  },
+});
